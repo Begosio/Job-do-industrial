84242bde423edc0c78a430a0a347509f90254b70
Luis <polardeveloper11@proton.me>
2026-08-20 17:42:24 -0300

    Criar estrutura basica

---
+++ src/main/java/com/has/etec/projectShelock/ProjectShelockApplication.java
@@ -0,0 +1,13 @@
+package com.has.etec.projectShelock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectShelockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectShelockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/has/etec/projectShelock/ProjectShelockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.has.etec.projectShelock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectShelockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
